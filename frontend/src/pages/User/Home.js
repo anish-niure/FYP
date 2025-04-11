bcd1072e31802770f8eb429a6bf5ec57e63f5d98
anish-niure <np03cs4a220021@heraldcollege.edu.np>
2025-04-11 19:32:04 +0000

    Add responsive styling for service cards and Book Now button hover effects

--- frontend/src/pages/User/Home.js
+++ frontend/src/pages/User/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import Header from '../../components/Header';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
@@ -44,6 +45,9 @@ const Home = ({ openModal }) => {
                                     <h3>{service.name}</h3>
                                     <p>{service.description}</p>
                                     <p>{service.priceRange}</p>
+                                    <Link to={`/booking?serviceId=${service._id}`} className="book-now-btn">
+                                        Book Now
+                                    </Link>
                                 </div>
                             ))
                         ) : (
